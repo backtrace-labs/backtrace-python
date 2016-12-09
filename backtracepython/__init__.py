78d33f7b149db46fc434186e2d9e6e4b342daec3
Andrew Kelley <akelley@backtrace.io>
2016-12-09 14:01:09 -0500

    Release 0.2.0

--- backtracepython/__init__.py
+++ backtracepython/__init__.py
@@ -13,7 +13,7 @@ __all__ = ["BacktraceReport", "initialize", "finalize", "terminate", "version", 
 
 class version:
     major = 0
-    minor = 1
+    minor = 2
     patch = 0
 
 version_string = "{}.{}.{}".format(version.major, version.minor, version.patch)
