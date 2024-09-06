9c763c554c0b497f4c1ee3d5ca37c2d812d27d63
kdysput <kdysput@backtrace.io>
2024-09-06 13:24:15 +0000

    Convert Darwis to Mac OS

--- backtracepython/attributes/system_attribute_provider.py
+++ backtracepython/attributes/system_attribute_provider.py
@@ -6,10 +6,17 @@ from backtracepython.attributes.attribute_provider import AttributeProvider
 class SystemAttributeProvider(AttributeProvider):
     def __init__(self):
         self.attributes = {
-            'uname.sysname': platform.system(),
+            'uname.sysname': self.getPlatformName(),
             'uname.version': platform.version(),
             'uname.release': platform.release()
         }
 
     def get(self):
         return self.attributes
+    
+    def getPlatformName(self): 
+        sys_name = platform.system()
+
+        if sys_name == 'Darwin':
+            return 'Mac OS'
+        return sys_name
