56a4a106bcc5efa227e72ada6c02ec8c73bfb241
kdysput <kdysput@backtrace.io>
2024-09-24 15:02:15 +0000

    Add unicode

--- backtracepython/attributes/report_data_builder.py
+++ backtracepython/attributes/report_data_builder.py
@@ -1,8 +1,5 @@
-import unicodedata
-
-
 class ReportDataBuilder:
-    primitive_types = (int, float, str, bool, bytes, type(None))
+    primitive_types = (int, float, str, bool, bytes, type(None), unicode)
 
     @staticmethod
     def get(provider_attributes):
@@ -14,6 +11,7 @@ class ReportDataBuilder:
             if isinstance(value, ReportDataBuilder.primitive_types):
                 attributes[key] = value
             else:
+                print("Adding {} as an annotation. Type {}".format(key, type(value)))
                 annotations[key] = value
 
         # Return both dictionaries
